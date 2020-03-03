7ba37282297b70cf753d16b581f94e3c299c44a1
yoshitakamimura <kix11974@yahoo.co.jp>
2020-03-03 18:51:00 +0900

    できました

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,17 +1,66 @@
-$(function(){
-
-  $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
+$(function(){ 
+  function buildHTML(message){
+   if ( message.image ) {
+     var html =
+      `<div class="message" data-message-id=${message.id}>
+         <div class="upper-message">
+           <div class="upper-message__talker">
+             ${message.user_name}
+           </div>
+           <div class="upper-message__date">
+             ${message.date}
+           </div>
+         </div>
+         <div class="lower-message">
+           <p class="lower-message__content">
+             ${message.content}
+           </p>
+         </div>
+         <img src=${message.image} >
+       </div>`
+     return html;
+   } else {
+     var html =
+      `<div class="message" data-message-id=${message.id}>
+         <div class="upper-message">
+           <div class="upper-message__talker">
+             ${message.user_name}
+           </div>
+           <div class="upper-message__date">
+             ${message.date}
+           </div>
+         </div>
+         <div class="lower-message">
+           <p class="lower-message__content">
+             ${message.content}
+           </p>
+         </div>
+       </div>`
+     return html;
+   };
+ }
+$('.new_message').on('submit', function(e){
+ e.preventDefault();
+ var formData = new FormData(this);
+ var url = $(this).attr('action')
+ $.ajax({
+   url: url,
+   type: "POST",
+   data: formData,
+   dataType: 'json',
+   processData: false,
+   contentType: false
+ })
+  .done(function(data){
+    var html = buildHTML(data);
+    $('.main-messages').append(html);
+    $('.main-messages').animate({ scrollTop: $('.main-messages')[0].scrollHeight});
+    $('form')[0].reset();
+    $(".submit-btn").prop("disabled", false);
+  })
+  .fail(function(){
+    alert('error');
   });
-
+  return false;
+  });
 });
