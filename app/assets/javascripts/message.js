8eca43766f550e227183c8870bbe82b01bc9a1a1
yoshitakamimura <kix11974@yahoo.co.jp>
2020-03-09 10:45:23 +0900

    automatic_updating

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -20,8 +20,7 @@ $(function(){
        </div>`
      return html;
    } else {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
+     var html = `<div class="message" data-message-id=${message.id}>
          <div class="upper-message">
            <div class="upper-message__talker">
              ${message.user_name}
@@ -39,6 +38,30 @@ $(function(){
      return html;
    };
  }
+
+ var reloadMessages = function() {
+  var last_message_id = $('.message:last').data("message-id");
+  $.ajax({
+    url: "api/messages",
+    type: 'get',
+    dataType: 'json',
+    data: {id: last_message_id}
+  })
+  .done(function(messages) {
+    if (messages.length !==0) {
+    var insertHTML = '';
+    $.each(messages, function(i, message) {
+      insertHTML += buildHTML(message)
+    });
+    $('.messages').append(insertHTML);
+    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
+   }
+  })
+  .fail(function() {
+    alert('error');
+  });
+};
+
 $('.new_message').on('submit', function(e){
  e.preventDefault();
  var formData = new FormData(this);
@@ -63,4 +86,8 @@ $('.new_message').on('submit', function(e){
   });
   return false;
   });
+  
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
